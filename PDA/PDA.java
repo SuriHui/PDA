a24019ea480d8b483e6e73536fe07f0b6c67742b
Suri Hui <suri.hui@mrjaffesclass.com>
2023-11-13 11:51:08 -0800

    double check

--- PDA/PDA.java
+++ PDA/PDA.java
@@ -22,37 +22,44 @@ public class PDA
      */
     public void runEventLoop() {
         boolean shouldContinue = true;
-        while (shouldContinue) {
-            System.out.println("How old are you?");
-            Scanner scanner = new Scanner(System.in);
-            Scanner scr = new Scanner(System.in);
-            int age = -1;
-            try {
-                age = scanner.nextInt();                    
-            } catch (InputMismatchException error) {
-                System.out.println("Please enter an integer");
-            }
-            if (age == 0)  {
-                shouldContinue = false;
-            } else if (age == -1) {
-                System.out.print("");
-            } else {
-                int LOWER_BOUND = 0;
-                int UPPER_BOUND = 0;
-                LOWER_BOUND = age/2 + 7;
-                UPPER_BOUND = (age - 7) * 2;
-                if (age < LOWER_BOUND) {
-                    System.out.println(age + " is too young!!");
-                } else if (age > UPPER_BOUND) {
-                    System.out.println(age + " is too young!!");
-                } else {
-                    System.out.println("You can date between " + LOWER_BOUND + " and " + UPPER_BOUND);
+        boolean question = true;
+        while (question) {
+            while (shouldContinue==true) {
+                System.out.println("How old are you?");
+                Scanner scanner = new Scanner(System.in);
+                int age = -1;
+                try {
+                    age = scanner.nextInt();                    
+                } catch (InputMismatchException error) {
+                    System.out.println("Please enter an integer");
                 }
-                System.out.println("Press 0 to quit the program");
-                System.out.println("Press 1 if you wish to continue?");
+                if (age == 0)  {
+                    shouldContinue = false;
+                } else if (age == -1) {
+                    System.out.print("");
+                } else {
+                    int LOWER_BOUND = 0;
+                    int UPPER_BOUND = 0;
+                    LOWER_BOUND = age/2 + 7;
+                    UPPER_BOUND = (age - 7) * 2;
+                    if (age < LOWER_BOUND) {
+                        System.out.println(age + " is too young!!");
+                    } else if (age > UPPER_BOUND) {
+                        System.out.println(age + " is too young!!");
+                    } else {
+                        System.out.println("You can date between " + LOWER_BOUND + " and " + UPPER_BOUND);
+                    }
+                    System.out.println("Press 0 to quit the program.");
+                    System.out.println("Press 1 if you wish to continue.");
+                    Scanner scr = new Scanner(System.in);
+                    int input = scr.nextInt();
+                    if (input == 0){
+                        System.out.println("Thank you for your time! I hope you have a lovely day!");
+                        shouldContinue = false;
+                    }
+                }
             }
         }
-        System.out.println("Thank you for your time! I hope you have a lovely day!");
     }
 
     /**
